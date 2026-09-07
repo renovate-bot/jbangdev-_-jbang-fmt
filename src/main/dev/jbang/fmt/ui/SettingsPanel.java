a31140fb06589d2c3d6edc9942df745f64044c13
Tamas Cservenak <tamas@cservenak.net>
2026-09-07 18:52:33 +0200

    Undo unrelated

--- src/main/dev/jbang/fmt/ui/SettingsPanel.java
+++ src/main/dev/jbang/fmt/ui/SettingsPanel.java
@@ -11,6 +11,8 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableRowSorter;
 import javax.swing.RowFilter;
 
+import dev.jbang.fmt.FmtLogger;
+
 /**
  * Panel for managing Eclipse formatter settings organized in logical groups.
  */
